e4910a4ccf268d1bc80e7f0d7af855c9c16429cd
makaracc <makara.chhaing@gmail.com>
2021-12-07 15:58:28 +1100

    just to test things

--- src/views/index.js
+++ src/views/index.js
@@ -9,6 +9,8 @@ import { Home } from './dependants/Home/Home';
 import { MobileMenu } from './dependants/mobileMenu/mobileMenu';
 import { Example } from './dependants/Example/Example';
 import { UsersManager } from './dependants/UsersManager/UsersManager';
+import { Tutorial } from './dependants/Tutorial/Tutorial';
+import { Good } from './dependants/Good/Good';
 export {
   Example,
   Login,
@@ -18,4 +20,7 @@ export {
   FourOFour,
   AuthCallback,
   UsersManager,
+  Tutorial,
+  //Add new one
+  Good
 };
